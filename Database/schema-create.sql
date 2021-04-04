f50e9737c0ff4a554cf8d8aa720c5a039ea0eb9d
Melvin Devos <melvin.devos@student.vives.be>
2021-04-04 23:34:48 +0200

    Added SQL file to create database schema

---
+++ Database/schema-create.sql
@@ -0,0 +1,38 @@
+CREATE database Heartbeats_dev;
+
+USE Heartbeats_dev;
+
+CREATE TABLE `patient` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `box_id` varchar(255),
+  `type_dementia` varchar(255),
+  `hr_tresh` int DEFAULT 90
+);
+
+CREATE TABLE `songs` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `yt_link` varchar(255) NOT NULL
+);
+
+CREATE TABLE `playlist_songs` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `playlist_id` int NOT NULL,
+  `song_id` int NOT NULL
+);
+
+CREATE TABLE `playlist` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `name` varchar(255) NOT NULL
+);
+
+ALTER TABLE `playlist` ADD FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
+
+ALTER TABLE `playlist_songs` ADD FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`);
+
+ALTER TABLE `playlist_songs` ADD FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
+
+
+
