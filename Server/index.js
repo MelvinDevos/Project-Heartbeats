36c798851a27d1be459559a13635a65f88354642
Melvin Devos <melvin.devos@student.vives.be>
2021-04-05 18:01:38 +0200

    Test commit

--- Server/index.js
+++ Server/index.js
@@ -10,14 +10,6 @@ app.use(express.json()); //Used to parse JSON bodies
 const auth = require("./Routes/auth");
 app.use("/auth", auth);
 
-app.get("/", (req, res) => {
-  pool.query("SHOW TABLES", (error, results) => {
-    if (error) return res.send(error.sqlMessage);
-
-    res.send(results);
-  });
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Heartbeats server listening on port: ${port}`);
